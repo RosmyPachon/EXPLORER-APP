70e1b44197c1a873e2bd10ef67cdbf1980138fda
RosmyPachon <rosmypachon@gmail.com>
2026-01-19 20:38:29 -0500

    feat: agregar estados de loading, error yempty en personajes

--- src/pages/CharactersPage.tsx
+++ src/pages/CharactersPage.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import CharacterCard from '../components/CharacterCard';
 
 function CharactersPage() {
+
+  const [isLoading] = useState(false);
+  const [error] = useState<string | null>(null);
+
     const characters = [
   {
     id: 1,
@@ -17,6 +22,33 @@ function CharactersPage() {
     status: "Alive",
   },
 ];
+
+if (isLoading) {
+    return (
+      <div className="p-6 text-center">
+        <p className="text-lg">Cargando personajes...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="p-6 text-center">
+        <p className="text-red-500 mb-4">{error}</p>
+        <button className="px-4 py-2 bg-black text-white rounded">
+          Reintentar
+        </button>
+      </div>
+    );
+  }
+
+  if (characters.length === 0) {
+    return (
+      <div className="p-6 text-center">
+        <p className="text-gray-500">No hay personajes para mostrar</p>
+      </div>
+    );
+  }
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-6">Personajes</h1>
